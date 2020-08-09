14bc72499a4d37de3953933f64afb1cdba3c00a9
surekrishna <surekrrishna@gmail.com>
2020-08-09 21:57:25 +0530

    created a new direactive in a better way approach dom

--- directives-examples/src/app/app.module.ts
+++ directives-examples/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighLightDirective
+    BasicHighLightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
