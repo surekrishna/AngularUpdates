499fb0319df0ea64204a37990dca37f02a682dc7
surekrishna <surekrrishna@gmail.com>
2020-08-09 22:03:45 +0530

    using @HostListener to interative more on mouseenter and mouseleave to effect on the backround

--- directives-examples/src/app/better-highlight/better-highlight.directive.ts
+++ directives-examples/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,7 +8,15 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'brown');
+  }
+
+  @HostListener('mouseenter') mousehover(eventData: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'brown');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
